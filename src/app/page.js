87746861207a79f232deb3416d68deb7ac8bf969
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2025-01-03 12:39:57 +0530

    Finished section-2
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,27 @@
+import Section2 from "@/components/section2/Section2";
 import { GoArrowUpRight } from "react-icons/go";
 
 export default function Page() {
   return (
-    <div className="mt-12 flex flex-col items-center justify-center relative mx-16">
-      <img
-        src="/ring.jpg"
-        alt="Ring Image"
-        className="w-[25%] object-cover top-0"
-      />
-      <div className="flex items-center justify-between w-full">
-        <button className="flex items-center justify-between w-64 font-normal text-xl py-3 px-6 border border-green-500 rounded-full hover:bg-green-600 hover:text-white transition-all duration-200">
-          Explore Now 
-          <GoArrowUpRight />
-        </button>
-        <h1 className="text-8xl w-full text-right  font-light">
-          Join Us in <span className="italic">building</span> <br /> a clean
-          Environment
-        </h1>
+    <div className="w-full">
+      <div className="mt-12 flex flex-col items-center justify-center relative mx-16">
+        <img
+          src="/ring.jpg"
+          alt="Ring Image"
+          className="w-[25%] object-cover top-0"
+        />
+        <div className="flex items-center justify-between w-full">
+          <button className="flex items-center justify-between w-64 font-normal text-xl py-3 px-6 border border-green-500 rounded-full hover:bg-green-600 hover:text-white transition-all duration-200">
+            Explore Now
+            <GoArrowUpRight />
+          </button>
+          <h1 className="text-8xl w-full text-right  font-light">
+            Join Us in <span className="italic">building</span> <br /> a clean
+            Environment
+          </h1>
+        </div>
       </div>
+      <Section2 />
     </div>
   );
 }
--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -4,52 +4,63 @@ import { IoSearch } from "react-icons/io5";
 import { TbBulbFilled } from "react-icons/tb";
 
 export default function Section2() {
+  const boxes = [
+    {
+      title: "Get Started Today",
+      iconName: <MdAddCircleOutline size={30} />, // Example using Material UI Icon
+      txt: "Create your free account and begin tracking your plastic usage. Join our community and make a difference!",
+      dark: false,
+    },
+    {
+      title: "Log Your Plastics",
+      iconName: <RiRecycleFill size={30} />, // Example, replace with your icon component
+      txt: "[VIEW DETAILS]", // This could be a Link component in React
+      dark: false,
+    },
+    {
+      title: "Waste Analysis",
+      iconName: <IoSearch size={30} />, // Example
+      txt: "[VIEW DETAILS]", //  Link component
+      dark: false,
+    },
+    {
+      title: "Reduction Tips",
+      iconName: <TbBulbFilled size={30} />, // Example
+      txt: "[VIEW DETAILS]", // Link component
+      dark: true,
+    },
+  ];
+
   return (
-    <div className="w-full h-fit flex justify-between items-center">
-      <div className="flex flex-col space-y-4 justify-start">
-        <p className="text-black font-mono text-3xl antialiased tracking-wide mb-5">
-          Track, Reduce, and <br /> Reimagine Your Plastic Use
-        </p>
+    <div className="w-full h-fit px-16 flex flex-col justify-start items-start space-x-5">
+      <p className="text-black font-mono text-3xl antialiased tracking-wide mb-5 ml-4">
+        Track, Reduce, and <br /> Reimagine Your Plastic Use
+      </p>
+
+      <div className="w-full flex justify-between items-center h-max space-x-5">
         <img
           src="/section2-image.jpg"
           alt="family cleaing as per 4 buckets"
-          className="w-40% rounded-2xl"
+          className="w-[50%] h-[400px] rounded-2xl object-cover"
         />
-      </div>
 
-      <div className="grid grid-cols-2">
-        {boxes.map((item, index) => {
-          <div className="flex justify-start space-y-2 border border-t-2 border-black">
-            <p className="text-black font-medium">{item.title}</p>
-            {item.iconName}
-
-            <p className="text-sm font-thin">{item.txt}</p>
-          </div>;
-        })}
+        <div className=" h-full grid grid-cols-2 grid-rows-2 gap-10">
+          {boxes.map((item, index) => (
+            <div
+              key={index}
+              className={`w-[300px] flex flex-col justify-between space-y-4 p-4 ${item.dark ? 'bg-[#2a2a2a] text-white rounded-lg' : "border-t-2 border-black text-black"}  `}
+            >
+              <div className="space-y-4">
+                <p className="font-normal">{item.title}</p>
+                {item.iconName}
+              </div>
+              <p className="text-sm font-light text-wrap text-start ">
+                {item.txt}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
-
-const boxes = [
-  {
-    title: "Get Started Today",
-    iconName: <MdAddCircleOutline />, // Example using Material UI Icon
-    txt: "Create your free account and begin tracking your plastic usage. Join our community and make a difference!",
-  },
-  {
-    title: "Log Your Plastics",
-    iconName: <RiRecycleFill />, // Example, replace with your icon component
-    txt: "[VIEW DETAILS]", // This could be a Link component in React
-  },
-  {
-    title: "Waste Analysis",
-    iconName: <IoSearch />, // Example
-    txt: "[VIEW DETAILS]", //  Link component
-  },
-  {
-    title: "Reduction Tips",
-    iconName: <TbBulbFilled />, // Example
-    txt: "[VIEW DETAILS]", // Link component
-  },
-];
