5219a3729b16f67e4cdb2ad78fd38474f01d8238
P SIDHARTH <philkhanasidharth14@gmail.com>
2025-01-03 12:09:34 +0530

    landing page v0.0.1
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,10 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const plus = Plus_Jakarta_Sans({
+const plus = Poppins({
   subsets: ["latin"],
+  weight : ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
 });
 
 export const metadata = {
