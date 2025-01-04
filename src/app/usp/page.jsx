e1ed821cc888104f0f5c25d70a0642245e9a8565
P SIDHARTH <philkhanasidharth14@gmail.com>
2025-01-04 11:10:44 +0530

    usp added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

---
+++ src/app/usp/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="bg-black flex flex-col items-center justify-center">
+        <img src="/user/usp.svg" alt="/" className="w-[80%] h-full" />
+    </div>
+  )
+}
